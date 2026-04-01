5de80039f6ddc139f3170cbe4402a51da3f6269f
noppogi114 <yukistore117@gmail.com>
2026-04-01 12:45:28 +0900

    update

--- upload.bat
+++ upload.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "G:\動画キャプチャー\動画編集\RUST\rust-roulette"
+git remote set-url origin https://github.com/noppogi114/rust-roulette-new.git
 git add points.txt bet_status.txt winner.txt
 git commit -m "Auto Update"
 git push origin main
