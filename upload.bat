238480a4ca0b798164a2b467b3d595785075ebba
noppogi114 <yukistore117@gmail.com>
2026-04-01 14:03:17 +0900

    Auto Update 2026/04/01 14:03:17.52

--- upload.bat
+++ upload.bat
@@ -1,18 +1,18 @@
 @echo off
 setlocal
 
-:: 1. GitHub Desktopが持っている「git.exe」の場所を直接指定します
-set GIT_PATH=%LOCALAPPDATA%\GitHubDesktop\bin\git.exe
+:: 1. 確定したGitのフルパスを直接指定
+set GIT_EXE="C:\Users\evaev\AppData\Local\GitHubDesktop\app-3.5.6\resources\app\git\mingw64\bin\git.exe"
 
-:: 2. フォルダ移動
+:: 2. 作業フォルダへ移動
 cd /d "G:\動画キャプチャー\動画編集\RUST\rust-roulette"
 
-:: 3. 実行（Gitの場所を直接叩きます）
-echo --- 自動更新を開始します ---
+echo --- GitHubへの送信処理を開始します ---
 
-"%GIT_PATH%" add .
-"%GIT_PATH%" commit -m "Auto Update %date% %time%"
-"%GIT_PATH%" push origin main
+:: 3. 確定パスを使用してGitコマンドを実行
+%GIT_EXE% add .
+%GIT_EXE% commit -m "Auto Update %date% %time%"
+%GIT_EXE% push origin main
 
-echo --- 処理終了 ---
+echo --- 処理が終了しました。窓を閉じるにはキーを押してください ---
 pause
