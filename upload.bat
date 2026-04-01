df51d0f08a9992ad239ad76cdd22560df4593063
noppogi114 <yukistore117@gmail.com>
2026-04-01 12:52:09 +0900

    fix bat

--- upload.bat
+++ upload.bat
@@ -1,7 +1,14 @@
 @echo off
-cd /d "G:\動画キャプチャー\動画編集\RUST\rust-roulette"
-git remote set-url origin https://github.com/noppogi114/rust-roulette-new.git
-git add points.txt bet_status.txt winner.txt
-git commit -m "Auto Update"
+:: パスを現在のフォルダ名に合わせて修正しました
+cd /d "G:\動画キャプチャー\動画編集\RUST\rust-roulette-new"
+
+:: 全ての変更をステージング
+git add .
+
+:: 自動コミット
+git commit -m "Auto Update %date% %time%"
+
+:: 強制的にメインブランチへ送信（Push）
 git push origin main
+
 exit
